bbed10f5e81522f37da4324963d79cce235a4dac
EDSON PAULA <ej91720@gmailcom>
2023-03-22 08:52:42 -0300

    Add project files.

---
+++ WebApp_Manha/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApp_Manha.Models;
+
+namespace WebApp_Manha.Controllers
+{
+    public class HomeController : Controller
+    {
+        //Atributo da classe homeContriler
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        //Action Rersult devolve uma tela (html)
+        public IActionResult Index()
+        {
+            return View();
+        }
+        //Mais uma tela no sistema 
+
+        public IActionResult BemVindo()
+        {
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
