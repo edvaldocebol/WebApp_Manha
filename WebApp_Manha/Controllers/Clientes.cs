e9d7ee93250227d23fb69fdc12d89a6947a93087
EDSON PAULA <ej91720@gmailcom>
2023-03-29 10:58:05 -0300

    Inicio do back end

--- WebApp_Manha/Controllers/Clientes.cs
+++ WebApp_Manha/Controllers/Clientes.cs
@@ -8,5 +8,34 @@ namespace WebApp_Manha.Controllers
         {
             return View();
         }
+
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+
+        public IActionResult Editar(int id)
+        {
+            return View();
+        }
+
+        public IActionResult Excluir(int id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Cadastro(string Nome, string Telefone)
+        {
+            if (string.IsNullOrEmpty(Nome))
+            {
+                TempData["Erro"] = "O Campo nome não pode estar em branco";
+            }
+
+            if (string.IsNullOrEmpty(Telefone)) {
+                TempData["Erro"] = "O Campo Telefone não estar em branco";
+            }
+            return View();
+        }
     }
 }
