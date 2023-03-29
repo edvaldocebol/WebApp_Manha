33fb633570ecee1be51950fc8a11855b7df87ace
EDSON PAULA <ej91720@gmailcom>
2023-03-29 09:09:09 -0300

    Tela de Lista de Clientes

---
+++ WebApp_Manha/Controllers/Clientes.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp_Manha.Controllers
+{
+    public class Clientes : Controller
+    {
+        public IActionResult Lista()
+        {
+            return View();
+        }
+    }
+}
